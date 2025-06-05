05d8588d5a236dd6b7c721bf563ca09312a05f85
Sachin Verma <ramasyasachin@gmail.com>
2025-06-05 09:01:07 +0530

    reset

--- app/(root)/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View>
-      <Text className="font-rubik-bold text-lg text-center w-full">Edit g app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
